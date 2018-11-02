cc87db08d91ed9c2523a5ccc20b5711f65cc1ded
KurkovBros <43685016+KurkovBros@users.noreply.github.com>
2018-11-02 16:10:21 +0300

    Добавил кнопки и поправил расположение элементов

--- CameraPlayer.java
+++ CameraPlayer.java
@@ -2,10 +2,13 @@
 
 package Camera;
 
+import javafx.scene.control.Button;
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 
 public class CameraPlayer extends Application{
@@ -24,8 +27,30 @@ public class CameraPlayer extends Application{
         Viewer view3 = new Viewer("file:///C:/Users/Александр/Documents/NetBeansProjects/Project_003/src/Camera/Movies/Slim.mp4");
         Viewer view4 = new Viewer("file:///C:/Users/Александр/Documents/NetBeansProjects/Project_003/src/Camera/Movies/Alergia.mp4");
         
-        FlowPane root = new FlowPane(view1.getGroup(), view2.getGroup(), view3.getGroup(), view4.getGroup()); // Создаем контейнер и добавляем в него четыре видеопотока
-        Scene scene = new Scene(root, 1280, 720, Color.BLACK); // Создаем сцену и добавляем в нее контейнер
+        // Создаем кнопки для выбора камер
+        Button button1 = new Button("Camera #1");
+        Button button2 = new Button("Camera #2");
+        Button button3 = new Button("Camera #3");
+        Button button4 = new Button("Camera #4");
+        
+        GridPane root = new GridPane(); // Создаем контейнер
+        root.setPadding(new Insets(10));
+        root.setHgap(10);
+        root.setVgap(10);
+        root.add(view1.getGroup(), 0, 0);
+        root.add(view2.getGroup(), 1, 0);
+        root.add(button1, 0, 1);
+        root.add(button2, 1, 1);
+        root.add(view3.getGroup(), 0, 2);
+        root.add(view4.getGroup(), 1, 2);
+        root.add(button3, 0, 3);
+        root.add(button4, 1, 3);
+        GridPane.setHalignment(button1, HPos.CENTER);
+        GridPane.setHalignment(button2, HPos.CENTER);
+        GridPane.setHalignment(button3, HPos.CENTER);
+        GridPane.setHalignment(button4, HPos.CENTER);
+
+        Scene scene = new Scene(root, 1310, 820, Color.BLACK); // Создаем сцену и добавляем в нее контейнер
         stage.setScene(scene); // Устанавливаем сцену в окно программы
         stage.show(); // Отображаем окно программы
     }
